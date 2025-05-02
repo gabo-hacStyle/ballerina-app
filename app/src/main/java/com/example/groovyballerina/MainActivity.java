614045718f8527fe264bf82f7e1a45edc9aa9b8f
Gabriel Fajardo @ Gabs <gabo2023brazil@gmail.com>
2025-05-02 18:05:31 -0500

    First view done

--- app/src/main/java/com/example/groovyballerina/MainActivity.java
+++ app/src/main/java/com/example/groovyballerina/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    lblconectado = findViewById(R.id.lblConectado);
+
 
         // 🚀 Inicializar e iniciar MQTT
         mqttManager = new MQTTManager(this);
